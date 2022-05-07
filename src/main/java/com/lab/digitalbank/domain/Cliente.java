bf12388b2a45a995a54f24c3b746517f75db41e4
razrodrigues <rafael.rodrigues@klab.com.br>
2022-05-07 12:10:52 -0300

    Criado classes de domínio

---
+++ src/main/java/com/lab/digitalbank/domain/Cliente.java
@@ -0,0 +1,15 @@
+package com.lab.digitalbank.domain;
+
+public class Cliente {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+
+    }
+}
---
+++ src/main/java/com/lab/digitalbank/domain/ContaCorrente.java
@@ -0,0 +1,15 @@
+package com.lab.digitalbank.domain;
+
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+
+    }
+
+}
